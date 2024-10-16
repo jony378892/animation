a9beeb25902ca905b96b0a9ac0242a361cc0ecfb
Jony <jony378892@gmail.com>
2024-10-16 22:47:28 +0600

    Done

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,9 +1,11 @@
 import { animate, query, sequence, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-practice',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.css'],
 
